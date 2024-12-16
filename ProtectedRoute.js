3bef76368586d629694eec51785148c87993298e
John Benedict Besinga <167271177+jangybaby@users.noreply.github.com>
2024-12-17 00:19:25 +0800

    Delete ProtectedRoute.js

--- ProtectedRoute.js
+++
@@ -1,9 +0,0 @@
-import React from 'react';
-import { Navigate } from 'react-router-dom';
-
-const ProtectedRoute = ({ isAuthenticated, children }) => {
-    const token = localStorage.getItem('token');
-    return isAuthenticated && token ? children : <Navigate to="/" />;
-};
-
-export default ProtectedRoute;
